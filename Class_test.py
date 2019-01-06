02f3ef5f618b02732fdba4e1f109cf8a32822a9b
Emad <emadghodrati@gmail.com>
2019-01-05 19:52:15 -0700

    The sort rule is added to the class

--- Class_test.py
+++ Class_test.py
@@ -3,7 +3,6 @@ Created on Thu Jan  3 16:32:48 2019
 
 @author: Emad
 """
-
 class Person:
     def __init__(self, name, age):
         self.name = name
@@ -13,13 +12,23 @@ class Person:
 #        print("Hello my name is " + self.name)
         print("Hello my name is {0} and I am {1} years old ".
               format(self.name,self.age))
+    
+    def __lt__(self,other):
+        return self.name < other.name
 
-p1 = Person("John", 36)
-p1.age = 40
-p1.myfunc()
-del p1.age
+
+p1 = [Person("John", 36),
+      Person("Bob",25)]
+p1[0].age = 40
+
+p1.sort()
+
+for per in p1:
+    per.myfunc()
+
+del p1[0].age
 
 try: 
-    print(p1.age)
+    print(p1[0].age)
 except Exception as e:
     print("We caught an error:",e)
